070090210e5601b1ce480f8f8928821b0618a2af
Paulo Ferrato <paulo.pallaferrato@gmail.com>
2025-04-05 19:27:21 -0300

    Inclusão de esquema de sprite para seguimento de lingua

--- Assets/Scripts/TongueSegment.cs
+++ Assets/Scripts/TongueSegment.cs
@@ -3,13 +3,43 @@ using UnityEngine;
 public class TongueSegment : MonoBehaviour 
 {
     public Vector2 direction;
+    public SpriteRenderer spriteRenderer;
     public Transform previous;
+    public Sprite vertical;
+    public Sprite horizontal;
+    public Sprite leftDown;
+    public Sprite rightDown;
+    public Sprite leftUp;
+    public Sprite rightUp;
 
 
     public void SetupSegment(Tongue tongue)
     {
         direction = tongue.direction;
         if(tongue.lastSegment != null) previous = tongue.lastSegment.transform;
+        spriteRenderer.sprite = PickSprite(tongue.direction);
         tongue.SetLastSegment(this, false);
+    }
+
+    public Sprite PickSprite(Vector2 target)
+    {
+        Vector2 origin = previous == null ? Vector2.down : previous.GetComponent<TongueSegment>().direction;
+
+        if (origin == Vector2.down && target == Vector2.up || origin == Vector2.up && target == Vector2.down)
+            return vertical;
+
+        if (origin == Vector2.down && target == Vector2.left || origin == Vector2.left && target == Vector2.down)
+            return leftDown;
+
+        if (origin == Vector2.down && target == Vector2.right || origin == Vector2.right && target == Vector2.down)
+            return rightDown;
+
+        if(origin == Vector2.up && target == Vector2.left || origin == Vector2.left && target == Vector2.up) 
+            return leftUp;
+
+        if(origin == Vector2.up && target == Vector2.right || origin == Vector2.right && target == Vector2.up) 
+            return rightUp;
+
+        return horizontal;
     }
 }
