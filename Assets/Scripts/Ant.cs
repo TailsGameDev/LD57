50514ed0e6d2b6b1fa011f41ee6ccfddfe5431df
caina <cainacc@hotmail.com>
2025-04-05 18:00:11 -0300

    score to get ants

--- Assets/Scripts/Ant.cs
+++ Assets/Scripts/Ant.cs
@@ -6,9 +6,25 @@ public class Ant : MonoBehaviour
     private Rigidbody2D rb2D = null;
     [SerializeField]
     private float speed = 0.0f;
+    [SerializeField]
+    private int score = 0;
 
     private void Awake()
     {
         rb2D.AddForce(transform.right * speed);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision2D)
+    {
+        if (collision2D.collider.tag == "Tongue Tip")
+        {
+            // Set new score on game state and UI
+            int currentScore = GameState.Instance.Score;
+            int newScore = currentScore + score;
+            GameState.Instance.Score = newScore;
+            UIManager.Instance.SetScore(newScore);
+
+            Destroy(gameObject);
+        }
     }
 }
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -2,4 +2,20 @@ using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
+    [SerializeField]
+    private TMPro.TextMeshProUGUI scoreText = null;
+
+    private static UIManager instance;
+
+    public static UIManager Instance { get => instance; }
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public void SetScore(int score)
+    {
+        scoreText.text = "score: " + score;
+    }
 }
